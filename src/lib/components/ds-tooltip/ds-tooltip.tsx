7d56d2770659837442d63a0f1f9d19ebb4e69b86
Michal Segal <msegal@drivenets.com>
2025-07-10 19:54:59 +0300

    AR-4359: Change header navigation to side-bar navigation

--- src/lib/components/ds-tooltip/ds-tooltip.tsx
+++ src/lib/components/ds-tooltip/ds-tooltip.tsx
@@ -4,19 +4,19 @@ import styles from './ds-tooltip.module.scss';
 import { DsTooltipProps } from './ds-tooltip.types';
 
 const DsTooltip: React.FC<DsTooltipProps> = ({ content, children }) => {
-  return (
-    <Tooltip.Provider delayDuration={200}>
-      <Tooltip.Root>
-        <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content className={styles.tooltip} side="top" align="center" sideOffset={4}>
-            {content}
-            <Tooltip.Arrow className={styles.arrow} />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
-  );
+	return (
+		<Tooltip.Provider delayDuration={200}>
+			<Tooltip.Root>
+				<Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
+				<Tooltip.Portal>
+					<Tooltip.Content className={styles.tooltip} side="right" align="center" sideOffset={4}>
+						{content}
+						<Tooltip.Arrow className={styles.arrow} />
+					</Tooltip.Content>
+				</Tooltip.Portal>
+			</Tooltip.Root>
+		</Tooltip.Provider>
+	);
 };
 
 export default DsTooltip;
