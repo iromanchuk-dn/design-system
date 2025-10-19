a48392a5bcbc0d972438a8879891027604e63a59
Kehat Sudri <ks4933@intl.att.com>
2025-10-19 10:28:41 +0300

    pr fixes

--- src/lib/utils/string.util.ts
+++ src/lib/utils/string.util.ts
@@ -1,3 +1,6 @@
 export const capitalize = (str: string) => {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+export const isEmptyValue = (value: unknown): boolean =>
+	value === null || value === undefined || value === '';
